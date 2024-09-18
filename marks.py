c8678688879af3e02ef828679a000f6078c77dc9
Sadmaan warshi <116273718+sadmaanwarshi@users.noreply.github.com>
2024-09-18 16:28:24 +0530

    marks.py

---
+++ marks.py
@@ -0,0 +1,10 @@
+n = int(input("Enter the number: "))
+
+if n >= 90:
+    print("You are under first grade")
+elif n >= 70:
+    print("You are under second grade")
+elif n >= 50:
+    print("You are under third grade")
+else:
+    print("Sorry, you have failed")
